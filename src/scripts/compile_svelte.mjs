d46f4bfe92f55bab265779dc41e14a351e065a6f
Roj <ez@roj.im>
2023-05-27 23:30:02 +0300

    oops

--- src/scripts/compile_svelte.mjs
+++ src/scripts/compile_svelte.mjs
@@ -1,20 +1,24 @@
 import * as path from "path";
-import { build } from "esbuild";
+import * as esbuild from "esbuild";
 import sveltePlugin from "esbuild-svelte";
 import { watch } from "chokidar";
 
 const mainFile = path.join("src", "webview_components", "main.js");
 
-await build({
-    entryPoints: [mainFile],
-    bundle: true,
-    outfile: "assets/webview.js",
-    plugins: [sveltePlugin()],
-    minify: true,
-    logLevel: "info",
-});
-
 watch(mainFile).on("change", (file) => {
   console.log(`${file} changed, rebuilding...`);
-  buildFile();
+  build();
 });
+
+function build() {
+  return esbuild.build({
+      entryPoints: [mainFile],
+      bundle: true,
+      outfile: "assets/webview.js",
+      plugins: [sveltePlugin()],
+      minify: true,
+      logLevel: "info",
+  });
+}
+
+build();
